19fb41770dc0ce5033ddd30e35bfe6f7555b913d
yuriruf <yurithedeveloper@gmail.com>
2020-10-15 15:54:11 -0300

    Page Management

--- Assets/Main/scripts/GameManager.cs
+++ Assets/Main/scripts/GameManager.cs
@@ -10,10 +10,26 @@ public class GameManager : MonoBehaviour
 	public Text keyCounter_txt;
 	//controllers
 	private int keysfound = 0;
+	public bool isGamePaused = false;
 	
 	//trigger
 	public bool isOnKeyTrigger_ = false;
+	public bool isOnPageTrigger_ = false;
 	public GameObject currentKey = null;
+	public GameObject currentPage = null;
+	public int currentPageNumber = 0;
+	
+	//pages
+	public GameObject page1;
+	public GameObject page2;
+	public GameObject page3;
+	public GameObject page4;
+	public GameObject page5;
+	public GameObject page6;
+	
+	
+	//Audio from pages
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +39,62 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pickUp_UI.SetActive(isOnKeyTrigger_);
+        pickUp_UI.SetActive(isOnKeyTrigger_ || isOnPageTrigger_);
+		
     }
+	public void closePage(){
+		page1.SetActive(false);
+		page2.SetActive(false);
+		page3.SetActive(false);
+		page4.SetActive(false);
+		page5.SetActive(false);
+		page6.SetActive(false);
+	}
 	public void pick(){
 		if(currentKey != null){
 			keysfound++;
 			isOnKeyTrigger_ = false;
 			Destroy(currentKey);
 			keyCounter_txt.text = keysfound+" keys found";
+		}
+		else if(currentPage != null){
+			currentPageNumber++;
+			isOnPageTrigger_ = false;
+			Destroy(currentPage);
+			switch(currentPageNumber){
+				case 1:
+					//Change audio clip and play
+					page1.SetActive(true);
+					isGamePaused = true;
+					break;
+				case 2:
+					//Change audio clip and play
+					page2.SetActive(true);
+					isGamePaused = true;
+					break;
+				case 3:
+					//Change audio clip and play
+					page3.SetActive(true);
+					isGamePaused = true;
+					break;
+				case 4:
+					//Change audio clip and play
+					page4.SetActive(true);
+					isGamePaused = true;
+					break;
+				case 5:
+					//Change audio clip and play
+					page5.SetActive(true);
+					isGamePaused = true;
+					break;
+				case 6:
+					//Change audio clip and play
+					page6.SetActive(true);
+					isGamePaused = true;
+					break;
+				 default:
+				break;
+			}
 		}
 	}
 }
---
+++ Assets/Main/scripts/PageTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageTrigger : MonoBehaviour
+{
+  //gm
+	private GameObject  _gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_gm = GameObject.FindGameObjectWithTag("GameManager");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			_gm.GetComponent<GameManager>().isOnPageTrigger_ = true;
+			_gm.GetComponent<GameManager>().currentPage = this.gameObject;
+		}
+	}
+	public void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Player"){
+			_gm.GetComponent<GameManager>().isOnPageTrigger_ = false;
+			_gm.GetComponent<GameManager>().currentKey = null;
+		}
+	}
+}
